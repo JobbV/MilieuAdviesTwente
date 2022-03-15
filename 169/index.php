2f02a47ad9456c1b1acd1ebc6ecd2050a7d3d8b2
MartijndeKort <565343@edu.rocmn.nl>
2022-03-15 09:25:58 +0100

    i have changed the html page

--- 169/index.php
+++ 169/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    sodium_crypto_kx_client_session_keys
     
     <title></title>
 </head>
