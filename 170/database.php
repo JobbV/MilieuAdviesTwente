83218d33753a69c1b5130f474863e32b67aca980
Job <566351@edu.rocmn.nl>
2022-03-18 10:52:53 +0100

    changes

--- 170/database.php
+++ 170/database.php
@@ -4,18 +4,21 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'world');
+define('DB_NAME', 'mat_servicedesk');
  
 // hier proberen we connectie te maken met de database
 // als er een error is geeft die dit aan 
 
-try {
-    $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
-    // attributen klaar maken
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    // hier geeft die een bericht met een error
-    die("Verbinding maken mislukt, error:. " . $e->getMessage());
+function maakPDO() {
+
+    try {
+        $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch (PDOException $e) {
+        die("Verbinding maken mislukt, error:. " . $e->getMessage());
+    }
+
 }
 
 ?>
---
+++ 170/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+<div class="form">
+    <form action="submit.php" method="post">
+    <br><textarea id="vraag" name="vraag" placeholder="Vraag" rows="4" cols="50"></textarea>
+    <br><textarea id="antwoord" name="antwoord" placeholder="Antwoord" rows="4" cols="50"></textarea>
+    <br><input type="submit" class="btn btn-success" name="btn" value="Verzenden">
+</div>
+</form>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+</body>
+</html>
