387ee4cd9b811d4868cf7a39f600e8913a1aebc2
Job <566351@edu.rocmn.nl>
2022-03-25 11:05:09 +0100

    aanpassingen toegepast

--- 169/database.php
+++ 169/database.php
@@ -1,23 +1,20 @@
 <?php
-
-// hier noem je welke database server, gebruikersnaam, wachtwoord en database naam
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'mat_servicedesk');
-
- // hier proberen we connectie te maken met de database
-// als er een error is geeft die dit aan
- function maakVragenpdo(){
- try {
+ 
+try {
     $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
-     // attributen klaar maken
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    return $pdo; 
-
 } catch (PDOException $e) {
-    // hier geeft die een bericht met een error
     die("Verbinding maken mislukt, error:. " . $e->getMessage());
 }
-}
+
+    $con = mysqli_connect("localhost","root","","mat_servicedesk");
+
+    if (mysqli_connect_errno()){
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+
+?>
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MilieuAdviesTwente
+
+stukje test
