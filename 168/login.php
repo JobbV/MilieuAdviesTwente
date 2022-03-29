4aa8afa0b2092c1da61eed8ecd0229209147151c
MartijndeKort <565343@edu.rocmn.nl>
2022-03-29 09:33:50 +0200

    ik heb een login pagina toegevoegd

--- 168/login.php
+++ 168/login.php
@@ -4,9 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
     <title>Document</title>
 </head>
 <body>
-    login page
+<div class="wrapper fadeInDown">
+  <div id="formContent">
+    <!-- Tabs Titles -->
+    <h2 class="active"> Inloggen </h2>
+    
+    <!-- Icon -->
+    <div class="fadeIn first">
+    </div>
+
+    <!-- Login Form -->
+    <form>
+      <input type="text" id="login" class="fadeIn second" name="login" placeholder="Gebruikersnaam">
+      <input type="text" id="password" class="fadeIn third" name="login" placeholder="Wachtwoord">
+      <input type="submit" class="fadeIn fourth" value="Log In">
+    </form>
+
+    <!-- Remind Passowrd -->
+    <div id="formFooter">
+      <a class="underlineHover" href="#">Wachtwoord vergeten?</a>
+    </div>
+
+  </div>
+</div>
 </body>
 </html>
